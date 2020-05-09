06fe49b8a95b35de25fb17843f71632faaf5b553
Mike Pham <mike.pham@uq.edu.au>
2020-05-09 11:07:53 +1000

    change all to exported type

--- icarus/type.go
+++ icarus/type.go
@@ -3,14 +3,14 @@ package icarus
 import "sync"
 
 type ThreadSafeGraph struct {
-	graph map[string][]string
+	Graph map[string][]string
 	*sync.RWMutex
 }
 
-func newThreadSafeGraph() *ThreadSafeGraph {
-	return &ThreadSafeGraph{graph: make(map[string][]string)}
+func NewThreadSafeGraph() *ThreadSafeGraph {
+	return &ThreadSafeGraph{Graph: make(map[string][]string)}
 }
 
 func (graph *ThreadSafeGraph) addEdge(from string, to string) {
-	graph.graph[from] = append(graph.graph[from], to)
+	graph.Graph[from] = append(graph.Graph[from], to)
 }
