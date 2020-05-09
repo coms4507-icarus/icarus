c0bc7d7c2fb111ce53536c945532763a1340238d
Mike Pham <mike.pham@uq.edu.au>
2020-05-09 11:09:17 +1000

    Add documentation

--- icarus/type.go
+++ icarus/type.go
@@ -2,15 +2,18 @@ package icarus
 
 import "sync"
 
+// Thread safe graph to work with goroutines
 type ThreadSafeGraph struct {
 	Graph map[string][]string
 	*sync.RWMutex
 }
 
+// Create a new thread safe graph
 func NewThreadSafeGraph() *ThreadSafeGraph {
 	return &ThreadSafeGraph{Graph: make(map[string][]string)}
 }
 
+// Add a new edge to this graph
 func (graph *ThreadSafeGraph) addEdge(from string, to string) {
 	graph.Graph[from] = append(graph.Graph[from], to)
 }
