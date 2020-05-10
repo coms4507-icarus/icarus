d11fe56e69ab981d41337b06a19301f3105caacf
Mike Pham <mike.pham@uq.edu.au>
2020-05-11 00:05:49 +1000

    Make graph attribute private

--- graph/graph.go
+++ graph/graph.go
@@ -4,34 +4,45 @@ import "sync"
 
 // Thread safe graph to work with goroutines
 type ThreadSafeGraph struct {
-	Graph map[string][]string
+	graph map[string][]string
 	lock  sync.RWMutex
 }
 
 // Create a new thread safe graph
 func NewThreadSafeGraph() *ThreadSafeGraph {
-	return &ThreadSafeGraph{Graph: make(map[string][]string)}
+	return &ThreadSafeGraph{graph: make(map[string][]string)}
 }
 
 // Add a new edge to this graph
 func (graph *ThreadSafeGraph) AddEdge(from string, to string) {
 	graph.lock.Lock()
-	graph.Graph[from] = append(graph.Graph[from], to)
+	graph.graph[from] = append(graph.graph[from], to)
 	graph.lock.Unlock()
 }
 
 // Check if a node exists
 func (graph *ThreadSafeGraph) NodeExists(node string) bool {
 	graph.lock.RLock()
-	_, ok := graph.Graph[node]
+	_, ok := graph.graph[node]
 	graph.lock.RUnlock()
 	return ok
+}
+
+// Check if a node exists
+func (graph *ThreadSafeGraph) Graph() map[string][]string {
+	var mapCopy map[string][]string
+	graph.lock.RLock()
+	for key, value := range graph.graph {
+		copy(mapCopy[key], value)
+	}
+	graph.lock.RUnlock()
+	return mapCopy
 }
 
 // get size of graph
 func (graph *ThreadSafeGraph) Size() int {
 	graph.lock.RLock()
-	size := len(graph.Graph)
+	size := len(graph.graph)
 	graph.lock.RUnlock()
 	return size
 }
